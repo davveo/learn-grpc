bf62babdfeac7bfae63fdb0815fe51be1f8b3248
davve <davve@qq.com>
2020-07-31 16:10:11 +0800

    完成客户端流模式

--- mode/clientstream/client/main.go
+++ mode/clientstream/client/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	echo "github.com/davveo/learn-grpc/pb"
+	"github.com/davveo/learn-grpc/utils"
+	"google.golang.org/grpc"
+	"log"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	streamingCount  = 10
+)
+
+func clientStream(c echo.EchoClient, message string) {
+	fmt.Printf("--- client streaming ---\n")
+	stream, err := c.ClientStream(context.Background())
+	if err != nil {
+		log.Fatalf("failed to call ClientStreamingEcho: %v\n", err)
+	}
+
+	for i := 0; i < streamingCount; i++ {
+		log.Printf("send message: %d", i)
+		if err := stream.Send(&echo.Request{Message: message}); err != nil {
+			log.Fatalf("failed to send streaming: %v\n", err)
+		}
+	}
+
+	r, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("failed to CloseAndRecv: %v\n", err)
+	}
+	fmt.Printf("response:\n")
+	fmt.Printf(" - %s\n\n", r.Message)
+}
+
 func main()  {
-	
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	utils.CheckError(err)
+	defer conn.Close()
+
+	rgc := echo.NewEchoClient(conn)
+	clientStream(rgc, "hello world")
 }
--- mode/clientstream/server/main.go
+++ mode/clientstream/server/main.go
@@ -1,5 +1,62 @@
 package main
 
-func main()  {
+import (
+	"context"
+	"flag"
+	"fmt"
+	echo "github.com/davveo/learn-grpc/pb"
+	"github.com/davveo/learn-grpc/utils"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+	"net"
+)
 
+var port = flag.Int("port", 50051, "the port to serve on")
+
+type service struct {
+	pb echo.UnimplementedEchoServer
+}
+
+func (s *service) ClientStream(stream echo.Echo_ClientStreamServer) error {
+	fmt.Printf("--- ClientStreamingEcho ---\n")
+
+	var message string
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			fmt.Printf("echo last received message\n")
+			return stream.SendAndClose(&echo.Response{Message: message})
+		}
+		message = in.Message
+		fmt.Printf("request received: %v, building echo\n", in)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+func (s *service) ServerStream(request *echo.Request, server echo.Echo_ServerStreamServer) error {
+	panic("implement me")
+}
+
+func (s *service) DoubleStream(server echo.Echo_DoubleStreamServer) error {
+	panic("implement me")
+}
+
+func (s *service) Simple(ctx context.Context, req *echo.Request) (*echo.Response, error) {
+	return &echo.Response{Message: req.Message}, nil
+}
+
+func main()  {
+	flag.Parse()
+	fmt.Printf("server starting on port %d...\n", *port)
+
+	s := grpc.NewServer()
+	echo.RegisterEchoServer(s, &service{})
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	utils.CheckError(err)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
