5bb9a6b921209d99350f8c976a3aa5d7dd26a7c6
davve <davve@qq.com>
2020-07-31 16:16:17 +0800

    服务端流模式

--- mode/serverstream/client/main.go
+++ mode/serverstream/client/main.go
@@ -1,5 +1,50 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	echo "github.com/davveo/learn-grpc/pb"
+	"github.com/davveo/learn-grpc/utils"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	streamingCount  = 10
+)
+
+func serverStream(c echo.EchoClient, message string) {
+
+	stream, err := c.ServerStream(context.Background(), &echo.Request{Message: message})
+	if err != nil {
+		log.Fatalf("failed to call ServerStreamingEcho: %v", err)
+	}
+
+	var rpcStatus error
+	fmt.Printf("response:\n")
+	for {
+		r, err := stream.Recv()
+		if err != nil {
+			rpcStatus = err
+			break
+		}
+		fmt.Printf(" - %s\n", r.Message)
+	}
+	if rpcStatus != io.EOF {
+		log.Fatalf("failed to finish server streaming: %v", rpcStatus)
+	}
+}
+
 func main()  {
-	
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure())
+	utils.CheckError(err)
+	defer conn.Close()
+
+	rgc := echo.NewEchoClient(conn)
+	serverStream(rgc, "hello world")
 }
