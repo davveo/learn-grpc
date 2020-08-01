7d6184cd33bd3ff4d5ce7bbbc4b2104ee8dfa4aa
davve <davve@qq.com>
2020-08-01 12:20:22 +0800

    tls加密传输

---
+++ encryption/TLS/client/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	echo "github.com/davveo/learn-grpc/pb"
+	"google.golang.org/grpc"
+
+	"github.com/davveo/learn-grpc/testdata"
+	"google.golang.org/grpc/credentials"
+)
+
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
+
+func callSimple(client echo.EchoClient, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	for i := 0; i < 10; i++ {
+		resp, err := client.Simple(ctx, &echo.Request{Message: message})
+		if err != nil {
+			log.Fatalf("client.Simple(_) = _, %v: ", err)
+		}
+		fmt.Println("Simple: ", resp.Message)
+	}
+
+}
+
+func main() {
+	flag.Parse()
+	creds, err := credentials.NewClientTLSFromFile(
+		testdata.Path("ca.pem"), "x.test.youtube.com")
+	if err != nil {
+		log.Fatalf("failed to load credentials: %v", err)
+	}
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := echo.NewEchoClient(conn)
+	callSimple(client, "hello world")
+}
---
+++ encryption/TLS/server/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	echo "github.com/davveo/learn-grpc/pb"
+	"github.com/davveo/learn-grpc/testdata"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+var port = flag.Int("port", 50051, "the port to serve on")
+
+type Service struct {
+	echo.UnimplementedEchoServer
+}
+
+func (s *Service) Simple(ctx context.Context, request *echo.Request) (*echo.Response, error) {
+	log.Println("收到来自客户端的请求...")
+	return &echo.Response{Message: request.Message}, nil
+}
+
+func (s *Service) ClientStream(server echo.Echo_ClientStreamServer) error {
+	panic("implement me")
+}
+
+func (s *Service) ServerStream(request *echo.Request, server echo.Echo_ServerStreamServer) error {
+	panic("implement me")
+}
+
+func (s *Service) DoubleStream(server echo.Echo_DoubleStreamServer) error {
+	panic("implement me")
+}
+
+func main() {
+	flag.Parse()
+	fmt.Printf("server starting on port %d...\n", *port)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	creds, err := credentials.NewServerTLSFromFile(
+		testdata.Path("server1.pem"),
+		testdata.Path("server1.key"))
+	if err != nil {
+		log.Fatalf("failed to create credentials: %v", err)
+	}
+	s := grpc.NewServer(grpc.Creds(creds))
+
+	echo.RegisterEchoServer(s, &Service{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
