1bf0e87fc2806d0f0e77ba8162c573a75f2c9b16
davve <davve@qq.com>
2020-08-01 12:41:34 +0800

    keepalive

--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module github.com/davveo/learn-grpc
 go 1.13
 
 require (
-	github.com/golang/protobuf v1.4.1
+	github.com/golang/protobuf v1.4.2
 	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
 	google.golang.org/grpc v1.31.0
+	google.golang.org/grpc/examples v0.0.0-20200731180010-8bec2f5d898f
 	google.golang.org/protobuf v1.25.0
 )
---
+++ keepalive/client/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	echo "github.com/davveo/learn-grpc/pb"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+)
+
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
+
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
+}
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := echo.NewEchoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
+	res, err := c.Simple(ctx, &echo.Request{Message: "keepalive demo"})
+	if err != nil {
+		log.Fatalf("unexpected error from Simple: %v", err)
+	}
+	fmt.Println("RPC response:", res)
+	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
+}
