e9a29e59d3ba878f72538340c8928a24cc471008
davve <davve@qq.com>
2020-08-01 12:24:56 +0800

    atls加密传输

--- encryption/ALTS/client/main.go
+++ encryption/ALTS/client/main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
 
+	"google.golang.org/grpc/credentials/alts"
+
+	echo "github.com/davveo/learn-grpc/pb"
+	"google.golang.org/grpc"
+)
+
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
+
+func callSimple(client echo.EchoClient, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	for i := 0; i < 10; i++ {
+		resp, err := client.Simple(ctx, &echo.Request{Message: message})
+		if err != nil {
+			log.Fatalf("client.Simple(_) = _, %v: ", err)
+		}
+		fmt.Println("Simple: ", resp.Message)
+	}
+
+}
+
+func main() {
+	flag.Parse()
+	creds := alts.NewClientCreds(alts.DefaultClientOptions())
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(creds), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := echo.NewEchoClient(conn)
+	callSimple(client, "hello world")
 }
--- encryption/ALTS/server/main.go
+++ encryption/ALTS/server/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
 
+	"google.golang.org/grpc/credentials/alts"
+
+	echo "github.com/davveo/learn-grpc/pb"
+	"google.golang.org/grpc"
+)
+
+var port = flag.Int("port", 50051, "the port to serve on")
+
+type Service struct {
+	echo.UnimplementedEchoServer
+}
+
+func (s *Service) Simple(ctx context.Context, request *echo.Request) (*echo.Response, error) {
+	log.Println("收到来自客户端的请求...")
+	return &echo.Response{Message: request.Message}, nil
+}
+
+func (s *Service) ClientStream(server echo.Echo_ClientStreamServer) error {
+	panic("implement me")
+}
+
+func (s *Service) ServerStream(request *echo.Request, server echo.Echo_ServerStreamServer) error {
+	panic("implement me")
+}
+
+func (s *Service) DoubleStream(server echo.Echo_DoubleStreamServer) error {
+	panic("implement me")
+}
+
+func main() {
+	flag.Parse()
+	fmt.Printf("server starting on port %d...\n", *port)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	altsTC := alts.NewServerCreds(alts.DefaultServerOptions())
+	s := grpc.NewServer(grpc.Creds(altsTC))
+
+	echo.RegisterEchoServer(s, &Service{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
